ad16fd5a0a56b30cdbe531760b0b15b583fb644b
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-07 22:12:46 -0300

    (#4) Criando estilos da home com as cores do tema dentro do context
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>

--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,1 +1,34 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.white};
+
+  h1 {
+    color: ${props => props.theme.black};
+    margin-bottom: 2rem;
+  }
+
+  button {
+    background-color: ${props => props.theme.primary03};
+    padding: 1rem;
+    border-radius: 10px;
+    color: ${props => props.theme.black};
+    font-weight: bold;
+    transition: 0.2s all;
+
+    &:hover {
+      box-shadow: 0 0 15px ${props => props.theme.primary03};
+      transform: scaleY(1.1) scaleX(1.1);
+    }
+  }
+
+  h4 {
+    color: ${props => props.theme.black};
+    margin-top: 2rem;
+  }
+`;
