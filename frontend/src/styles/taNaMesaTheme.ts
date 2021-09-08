df05f95e7dd73e2564b06ab085b0d4eca8bc2a51
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-07 22:10:41 -0300

    (#4) Adicionando paleta de cores da identidade visual
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>

---
+++ frontend/src/styles/taNaMesaTheme.ts
@@ -0,0 +1,16 @@
+export default {
+  type: 'default',
+  primary01: '#8E3031',
+  primary02: '#7E2726',
+  primary03: '#B96739',
+  primary04: '#A45E4D',
+
+  secondaryYellow: '#E7E400',
+  secondaryRed: '#D62631',
+  secondaryWine: '#9E1D25',
+  secondaryGreen: '#2BB426',
+  secondaryLightGreen: '#8AED87',
+
+  black: '#000000',
+  white: '#FFFFFF',
+};
