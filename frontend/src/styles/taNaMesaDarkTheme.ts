3863a61b8c1568cbcff04d3320fd2ffc9a9e91fb
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-07 22:10:55 -0300

    (#4) Adicionando cores do dark mode
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>

---
+++ frontend/src/styles/taNaMesaDarkTheme.ts
@@ -0,0 +1,16 @@
+export default {
+  type: 'dark',
+  primary01: '#FF0000',
+  primary02: '#5E2129',
+  primary03: '#FFA500',
+  primary04: '#A45E4D40',
+
+  secondaryYellow: '#E7E400',
+  secondaryRed: '#D62631',
+  secondaryWine: '#9E1D25',
+  secondaryGreen: '#2BB426',
+  secondaryLightGreen: '#8AED87',
+
+  black: '#FFFFFF',
+  white: '#000000',
+};
