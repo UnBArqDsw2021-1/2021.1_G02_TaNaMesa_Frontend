915fd00495e39f8a0a887194c1b0d3c25c557ece
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-11 23:29:09 -0300

    (#5) Adicionando estilos do componente da NavBar
    Co-authored-by: ServidioEC <52542729+ServidioEC@users.noreply.github.com>

--- frontend/src/components/NavBar/styles.ts
+++ frontend/src/components/NavBar/styles.ts
@@ -3,5 +3,38 @@ import styled from 'styled-components';
 export const NavBarContainer = styled.div`
   display: flex;
   width: 100%;
-  background-color: #C1C1;
+  height: 7rem;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${props => props.theme.white};
+  box-shadow: 0.4rem 0 4rem ${props => `${props.theme.black}25`};
+
+  section {
+    display: flex;
+    align-items: center;
+
+    svg {
+      width: 3.2rem;
+      height: 3.2rem;
+      color: ${props => props.theme.black};
+      margin: 0 0.9rem 0 3rem;
+    }
+
+    span {
+      font-size: 2.4rem;
+    }
+  }
+`;
+
+export const NameContainer = styled.div`
+  display: flex;
+  border: 2px solid ${props => props.theme.gray};
+  border-radius: 15px;
+  padding: 1rem 4rem;
+  margin-right: 2.5rem;
+
+  span {
+    font-size: 2.5rem;
+    font-weight: 500;
+  }
 `;
