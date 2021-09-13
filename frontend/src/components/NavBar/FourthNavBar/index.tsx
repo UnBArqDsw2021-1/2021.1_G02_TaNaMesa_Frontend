7d23150fb72e62eb27ca3032c067e3d50c9cffaf
ServidioEC <52542729+ServidioEC@users.noreply.github.com>
2021-09-12 23:00:43 -0300

    (#5) Adicionando quarto NavBar
    Co-authored-by: Lucas Boaventura <lucasxboaventura@hotmail.com>

---
+++ frontend/src/components/NavBar/FourthNavBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import { NavBarContainer } from 'components/NavBar/styles';
+
+const FourthNavBar: React.FC = () => {
+  return (
+    <NavBarContainer>
+      
+    </NavBarContainer>
+  );
+};
+
+export default FourthNavBar;
