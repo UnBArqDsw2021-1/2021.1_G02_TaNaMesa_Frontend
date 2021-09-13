ed58debb137569e248e37ec5f6c7f9a93b0c02e0
ServidioEC <52542729+ServidioEC@users.noreply.github.com>
2021-09-12 23:01:33 -0300

    (#5) Alterando estilos da NavBar
    Co-authored-by: Lucas Boaventura <lucasxboaventura@hotmail.com>

--- frontend/src/components/NavBar/styles.ts
+++ frontend/src/components/NavBar/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const NavBarContainer = styled.div`
   display: flex;
   width: 100%;
-  height: 7rem;
+  height: 5rem;
   align-items: center;
   justify-content: space-between;
   background-color: ${props => props.theme.white};
@@ -14,14 +14,14 @@ export const NavBarContainer = styled.div`
     align-items: center;
 
     svg {
-      width: 3.2rem;
-      height: 3.2rem;
+      width: 1.5rem;
+      height: 1.5rem;
       color: ${props => props.theme.black};
-      margin: 0 0.9rem 0 3rem;
+      margin: 0 0.6rem 0 3rem;
     }
 
     span {
-      font-size: 2.4rem;
+      font-size: 1.5rem;
     }
   }
 `;
@@ -30,11 +30,11 @@ export const NameContainer = styled.div`
   display: flex;
   border: 2px solid ${props => props.theme.gray};
   border-radius: 15px;
-  padding: 1rem 4rem;
+  padding: 0.6rem 1.5rem;
   margin-right: 2.5rem;
 
   span {
-    font-size: 2.5rem;
+    font-size: 1.5rem;
     font-weight: 500;
   }
 `;
