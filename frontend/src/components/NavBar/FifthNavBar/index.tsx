a19efe807e0799fd558061761495f0a132dc137f
ServidioEC <52542729+ServidioEC@users.noreply.github.com>
2021-09-12 23:30:19 -0300

    (#5) Adicionando quinto NavBar
    Co-authored-by: Lucas Boaventura <lucasxboaventura@hotmail.com>

---
+++ frontend/src/components/NavBar/FifthNavBar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { NavBarContainer } from 'components/NavBar/styles';
+
+const FifthNavBar: React.FC = () => {
+  return (
+    <NavBarContainer>
+      <section>
+        <span>Mesa</span>
+      </section>
+
+    </NavBarContainer>
+  );
+};
+
+export default FifthNavBar;
