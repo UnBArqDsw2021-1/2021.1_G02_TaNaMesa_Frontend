85844771eb8a60bb6b9f4de5434b385d5a713ba1
Emily Sousa <diassousa@yahoo.com.br>
2021-09-16 23:27:32 -0300

    (#15) adicionando pasta de modal

---
+++ frontend/src/components/Modal/AlertModal/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from 'react';
+import { Container } from '../AlertModal/styles';
+
+type Props = {
+  title: string;
+  visible: boolean;
+  onClose: (event: any) => void;
+};
+
+const AlertModal: React.FC<Props> = ({ title, visible, onClose }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const escFunction = (event: any) => {
+      if (event.keyCode === 27) onClose(event);
+    };
+
+    const handleClick = (event: any) => {
+      if (modalRef.current === event.target) onClose(event);
+    };
+
+    document.addEventListener('keydown', escFunction, false);
+    document.addEventListener('mousedown', handleClick);
+    document.addEventListener('touchstart', handleClick);
+
+    return () => {
+      document.removeEventListener('keydown', escFunction, false);
+      document.removeEventListener('mousedown', handleClick);
+      document.removeEventListener('touchstart', handleClick);
+    };
+  }, [onClose]);
+  return (
+    <Container className={visible ? '-visible' : ''} ref={modalRef}>
+      <div className="close-button">
+        <button type="button" onClick={onClose}>
+          x
+        </button>
+      </div>
+      <div className="content">{title}</div>
+    </Container>
+  );
+};
+
+export default AlertModal;
