c21fbaf4f95e195cad0f78814979ccb79fee2e74
Herick Portugues <herick.portugues@gmail.com>
2021-09-17 14:18:05 -0300

    (#12) Adiciona rota para a tela de pedidos
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Pedido from '../pages/Pedido';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/pedido" component={Pedido} />
       </Switch>
     </BrowserRouter>
   );
