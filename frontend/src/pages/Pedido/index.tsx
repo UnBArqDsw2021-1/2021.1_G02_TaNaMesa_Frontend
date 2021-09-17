96924ec8d82eeb4b1a81772fc4fd574172aae9c7
Herick Portugues <herick.portugues@gmail.com>
2021-09-17 15:56:37 -0300

    (#12) Cria itens da listagem do pedido
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

--- frontend/src/pages/Pedido/index.tsx
+++ frontend/src/pages/Pedido/index.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTheme } from 'styled-components';
 
-import { Container, ContainerOrderList, ContainerTotal } from './styles';
+import { Container, ContainerOrderList, ContainerTotal, Item, OrderList } from './styles';
+
+interface Item {
+  quantity: number;
+  name: string;
+  price: number;
+  obs?: string;
+}
 
 const Pedido: React.FC = () => {
   const theme = useTheme();
 
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    setItems([
+      { "quantity": 1, "name": "Petit Gateu", "price": 21.00, "obs": "" },
+      { "quantity": 2, "name": "Suco de Laranja", "price": 20.00, "obs": "Suco sem açúcar" },
+      { "quantity": 1, "name": "Creme de Maracujá", "price": 12.00, "obs": "" },
+    ]);
+  }, []);
+
   return (
     <Container>
       <ContainerOrderList>
-        <h1>Tela de Pedido</h1>
+        <h1>Brenda</h1>
+        <OrderList>
+          {items.map((item) => {
+            return (
+              <Item>
+                <h2>
+                  {item.quantity}x {item.name}
+                  {item.obs ? <h3>Observação: {item.obs}</h3> : <br />}
+                </h2>
+                <h2>R$ {item.price.toFixed(2)}</h2>
+              </Item>
+            );
+          })}
+        </OrderList>
       </ContainerOrderList>
       <ContainerTotal>
         <h1>Total</h1>
--- frontend/src/pages/Pedido/styles.ts
+++ frontend/src/pages/Pedido/styles.ts
@@ -6,6 +6,13 @@ export const Container = styled.div`
   flex-direction: row;
   justify-content: space-between;
   background-color: ${props => props.theme.white};
+
+  h1 {
+    font-size: 3rem;
+    font-weight: 600;
+    color: ${props => props.theme.black};
+    margin-bottom: 0;
+  }
 `;
 
 export const ContainerOrderList = styled.div`
@@ -14,16 +21,43 @@ export const ContainerOrderList = styled.div`
   width: 70%;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  padding-top: 2rem;
+`;
+
+export const OrderList = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  margin: 1rem 0;
+  justify-content: space-between;
+
+  h2 {
+    margin: 0;
+    font-weight: 600;
+    font-size: 1.8rem;
+    color: ${props => props.theme.black};
+  }
+
+  h3 {
+    font-weight: 600;
+    font-size: 1.6rem;
+    margin: 0.3rem 0 0 3rem;
+    color: ${props => props.theme.black};
+  }
 `;
 
 export const ContainerTotal = styled.div`
+  width: 30%;
   display: flex;
   height: 100vh;
-  width: 30%;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
   padding: 2rem 3rem;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
   background-color: ${props => props.theme.gray};
 `;
