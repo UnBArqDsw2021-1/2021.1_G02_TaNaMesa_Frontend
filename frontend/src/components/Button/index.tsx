32d22621361ef878c8575e128ce9b9f8ce5a49a3
Herick Portugues <herick.portugues@gmail.com>
2021-09-21 17:53:34 -0300

    (#8) Corrige erros do eslint e adiciona hover no botao
    Co-authored-by: Lucas Boaventura <lucasxboaventura@hotmail.com>

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
 
-import { NormalButton } from './styles';
+import { NormalButton } from 'components/Button/styles';
 
-interface ButtonProps {
-    color: string;
-    width?: string;
-    padding?: string;
-    enabled?: boolean;
-    onClick: () => void;
-}
+type ButtonProps = {
+  color: string;
+  width?: string;
+  padding?: string;
+  enabled?: boolean;
+  onClick: () => void;
+};
 
 const Button: React.FC<ButtonProps> = ({
-    children,
-    color,
-    width = "10rem",
-    padding = "1.5rem",
-    enabled = true,
-    onClick,
+  children,
+  color,
+  width,
+  padding,
+  enabled,
+  onClick,
 }) => {
-    return (
-        <NormalButton
-            color={color}
-            width={width}
-            padding={padding}
-            enabled={enabled}
-            onClick={onClick}
-        >
-            {children}
-        </NormalButton >
-    );
+  return (
+    <NormalButton
+      color={color}
+      width={width}
+      padding={padding}
+      enabled={enabled}
+      onClick={onClick}
+    >
+      {children}
+    </NormalButton>
+  );
+};
+
+Button.defaultProps = {
+  width: '15rem',
+  padding: '1.5rem',
+  enabled: true,
 };
 
 export default Button;
--- frontend/src/components/Button/styles.ts
+++ frontend/src/components/Button/styles.ts
@@ -1,19 +1,24 @@
 import styled from 'styled-components';
 
 interface NormalButtonProps {
-    width: string;
-    padding: string;
-    enabled: boolean;
+  width?: string;
+  padding?: string;
+  enabled?: boolean;
 }
 
 export const NormalButton = styled.button<NormalButtonProps>`
-    padding: ${props => props.padding};
-    font-weight: 600;
-    text-align: center;
-    border-radius: 2.0rem;
-    width: ${props => props.width};
-    color: ${props => props.theme.white};
-    background-color: ${props => props.color};
-    opacity: ${props => (props.enabled ? 1 : 0.6)};
-    cursor: ${props => (props.enabled ? "pointer" : "default")};
+  padding: ${props => props.padding};
+  font-weight: 600;
+  text-align: center;
+  border-radius: 2rem;
+  width: ${props => props.width};
+  color: ${props => props.theme.white};
+  background-color: ${props => props.color};
+  opacity: ${props => (props.enabled ? 1 : 0.7)};
+  cursor: ${props => (props.enabled ? 'pointer' : 'default')};
+  transition: 0.2s all;
+
+  &:hover {
+    opacity: ${props => (props.enabled ? 0.9 : 0.7)};
+  }
 `;
