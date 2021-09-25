98b7d5f4caa268778bdecd3559e7f71af9bd1d77
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-25 14:24:23 -0300

    (#10) Adicionando componente de SideBar
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

---
+++ frontend/src/components/SideBar/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+import SideBarButton from 'components/SideBarButton';
+import logo from 'assets/logo-contorno.png';
+import sideBarObjects from './sideBarObjects';
+
+import { Container } from './styles';
+
+interface SideBarProps {
+  page: 'menu-user' | string;
+}
+
+interface SideBarObjects {
+  icon: string;
+  text: string;
+}
+
+const SideBar: React.FC<SideBarProps> = ({ page }) => {
+  const [menuOptions, setMenuOptions] = useState<SideBarObjects[]>([]);
+
+  const getMenuOptions = (): void => {
+    switch (page) {
+      case 'menu-user':
+        setMenuOptions(sideBarObjects.menuUserButtons);
+        break;
+
+      default:
+        setMenuOptions(sideBarObjects.menuUserButtons);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    getMenuOptions();
+  }, []);
+
+  return (
+    <Container>
+      <img src={logo} alt="Tá Na Mesa" />
+
+      {menuOptions.map(option => {
+        return <SideBarButton text={option.text} icon={option.icon} />;
+      })}
+    </Container>
+  );
+};
+
+export default SideBar;
