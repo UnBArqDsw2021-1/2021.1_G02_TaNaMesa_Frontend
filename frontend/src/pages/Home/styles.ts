145e6a9a9b8aef0833a435d8d5d72ef0675ed92a
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-25 14:27:11 -0300

    (#10) Resetando alguns estilos da Home que não serão mais usados
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -4,8 +4,6 @@ export const Container = styled.div`
   display: flex;
   height: 100vh;
   flex-direction: column;
-  align-items: center;
-  /* justify-content: center; */
   background-color: ${props => props.theme.white};
 
   h1 {
