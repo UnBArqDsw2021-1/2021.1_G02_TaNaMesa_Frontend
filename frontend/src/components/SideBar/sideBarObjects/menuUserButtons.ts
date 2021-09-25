c00a56593adc0cb5124c2ed6e5f45d3e603194a0
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-25 14:25:27 -0300

    (#10) Adicionando userMenuOptions da SideBar
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

---
+++ frontend/src/components/SideBar/sideBarObjects/menuUserButtons.ts
@@ -0,0 +1,25 @@
+import imgBurguer from 'assets/SideBar/smbolo_hamburguer 1.png';
+import imgDrink from 'assets/SideBar/simbolo_bebida 1.png';
+import imgFries from 'assets/SideBar/simbolo batata 1.png';
+import imgDessert from 'assets/SideBar/simbolo_sobremesa 1.png';
+
+const menuUserButtons = [
+  {
+    icon: imgBurguer,
+    text: 'Burguers',
+  },
+  {
+    icon: imgFries,
+    text: 'Batatas',
+  },
+  {
+    icon: imgDrink,
+    text: 'Bebidas',
+  },
+  {
+    icon: imgDessert,
+    text: 'Sobremesas',
+  },
+];
+
+export default menuUserButtons;
