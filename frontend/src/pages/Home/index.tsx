21f10a60530ce8819f24b38bfe5f5cbd887d2b55
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-25 14:26:38 -0300

    (#10) Adicionando  SideBar na Home
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { useUserTheme } from 'hooks/theme';
 
 // import Loading from 'components/Loading';
-import MenuItem from 'components/MenuItem';
 import NavBar from 'components/NavBar';
+import SideBar from 'components/SideBar';
 
 import { Container, StyledButton } from 'pages/Home/styles';
 
@@ -14,30 +14,7 @@ const Home: React.FC = () => {
   return (
     <Container>
       <NavBar center="Mesa" tableTitle="Mesa 1" />
-      <NavBar left="Voltar" center="Mesa" tableTitle="Mesa 1" />
-      <NavBar left="Voltar" right="Pedido" />
-      <NavBar left="Voltar" right="Pedido" isOrderEmpty />
-      <NavBar left="Voltar" />
-      <NavBar left="Voltar" center="Pedidos" />
-
-      <h1>Tá na Mesa</h1>
-
-      <StyledButton type="button" onClick={switchTheme}>
-        Trocar tema
-      </StyledButton>
-
-      <h4>{theme.type}</h4>
-
-      <MenuItem
-        // id={0}
-        image="https://blog.finamac.com/wp-content/uploads/2019/10/309956-como-oferecer-os-melhores-sabores-de-milkshake-para-os-clientes-1280x640.jpg"
-        name="Milkshake"
-        price="16.90"
-        // discount={0}
-        description="Disponível nos sabores morango, chocolate, oreo e creme de avelã."
-      />
-
-      {/* <Loading /> */}
+      <SideBar page="menu-user" />
     </Container>
   );
 };
