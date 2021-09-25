32708c92ec25372db8971b5897d1d308ee872b52
Herick Portugues <herick.portugues@gmail.com>
2021-09-25 19:34:06 -0300

    (#12) Finalizando listagem de pedidos e botões de comandas
    Co-authored-by: Eduarda Servidio <dudaservidio@gmail.com>

--- frontend/src/pages/Pedido/index.tsx
+++ frontend/src/pages/Pedido/index.tsx
@@ -1,7 +1,20 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import React, { useState, useEffect } from 'react';
 import { useTheme } from 'styled-components';
 
-import { Container, ContainerOrderList, ContainerTotal, Item, OrderList} from './styles';
+import NavBar from 'components/NavBar';
+import Button from 'components/Button';
+
+import {
+  Container,
+  ContainerOrderList,
+  ContainerClientsOrder,
+  ButtonsContainer,
+  ContainerTotal,
+  ContainerLeft,
+  Item,
+  OrderList,
+} from './styles';
 
 interface Item {
   quantity: number;
@@ -17,37 +30,87 @@ const Pedido: React.FC = () => {
 
   useEffect(() => {
     setItems([
-      { "quantity": 1, "name": "Petit Gateu", "price": 21.00, "obs": "" },
-      { "quantity": 2, "name": "Suco de Laranja", "price": 20.00, "obs": "Suco sem açúcar" },
-      { "quantity": 1, "name": "Creme de Maracujá", "price": 12.00, "obs": "" },
+      { quantity: 1, name: 'Petit Gateu', price: 21.0, obs: '' },
+      {
+        quantity: 2,
+        name: 'Suco de Laranja',
+        price: 20.0,
+        obs: 'Suco sem açúcar',
+      },
+      { quantity: 1, name: 'Creme de Maracujá', price: 12.0, obs: '' },
+      { quantity: 1, name: 'Petit Gateu', price: 21.0, obs: '' },
+      {
+        quantity: 2,
+        name: 'Suco de Laranja',
+        price: 20.0,
+        obs: 'Suco sem açúcar',
+      },
+      { quantity: 1, name: 'Creme de Maracujá', price: 12.0, obs: '' },
     ]);
   }, []);
 
   return (
-    <Container>
-      <ContainerOrderList>
-        <h1>Brenda</h1>
-        <OrderList>
-          {items.map((item) => {
-            return (
-              <Item>
-                <h2>
-                  {item.quantity}x {item.name}
-                  {item.obs ? <h3>Observação: {item.obs}</h3> : <br />}
-                </h2>
-                <h2>R$ {item.price.toFixed(2)}</h2>
-              </Item>
-            );
-          })}
-        </OrderList>
-        <div id="linha-horizontal"></div>
-        <p>Comandas abertas:</p>
-      </ContainerOrderList>
-      <ContainerTotal>
-        <h1>Total</h1>
-        <div id="linha-horizontal"></div>
-      </ContainerTotal>
-    </Container>
+    <>
+      <NavBar left="Voltar" center="Mesa" tableTitle="Mesa 1" />
+      <Container>
+        <ContainerLeft>
+          <ContainerOrderList>
+            <h1>Brenda</h1>
+            <OrderList>
+              {items.map(item => {
+                return (
+                  <Item>
+                    <h2>
+                      {item.quantity}x {item.name}
+                      {item.obs ? <h3>Observação: {item.obs}</h3> : <br />}
+                    </h2>
+                    <h2>R$ {item.price.toFixed(2)}</h2>
+                  </Item>
+                );
+              })}
+            </OrderList>
+          </ContainerOrderList>
+          <ContainerClientsOrder>
+            <p>Comandas abertas:</p>
+            <ButtonsContainer>
+              <Button
+                color={theme.primary02}
+                width="10rem"
+                padding="1rem"
+                onClick={() => {}}
+              >
+                Sérgio
+              </Button>
+              <Button
+                color={theme.primary02}
+                width="10rem"
+                padding="1rem"
+                onClick={() => {}}
+              >
+                Brenda
+              </Button>
+              <Button
+                color={theme.secondaryGreen}
+                padding="1rem"
+                onClick={() => {}}
+              >
+                Criar Comanda
+              </Button>
+            </ButtonsContainer>
+          </ContainerClientsOrder>
+        </ContainerLeft>
+        <ContainerTotal>
+          <h1>Total</h1>
+          <h2>Brenda R$ 10.00</h2>
+          <h2>Sérgio R$ 15.00</h2>
+          <div id="linha-horizontal" />
+          <h1>Total R$ 25.00</h1>
+          <Button color={theme.primary02} width="30rem" onClick={() => {}}>
+            Pedir Conta
+          </Button>
+        </ContainerTotal>
+      </Container>
+    </>
   );
 };
 
--- frontend/src/pages/Pedido/styles.ts
+++ frontend/src/pages/Pedido/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  height: 100vh;
+  height: calc(100vh - 6rem);
   flex-direction: row;
   justify-content: space-between;
   background-color: ${props => props.theme.white};
@@ -14,11 +14,18 @@ export const Container = styled.div`
     margin-bottom: 0;
   }
 
-  #linha-horizontal {
-    margin-top: 18rem;
-    width: 100%;
-    border: 1.5px solid ${props => props.theme.gray};
-    background-color: ${props => props.theme.gray};
+  h2 {
+    margin: 0 1.5rem;
+    font-weight: 600;
+    font-size: 1.8rem;
+    color: ${props => props.theme.black};
+  }
+
+  h3 {
+    font-weight: 600;
+    font-size: 1.6rem;
+    margin: 0.3rem 0 0 3rem;
+    color: ${props => props.theme.black};
   }
 
   p {
@@ -30,13 +37,19 @@ export const Container = styled.div`
   }
 `;
 
-export const ContainerOrderList = styled.div`
+export const ContainerLeft = styled.div`
   display: flex;
-  height: 100vh;
   width: 70%;
   flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const ContainerOrderList = styled.div`
+  display: flex;
+  flex-direction: column;
   align-items: center;
-  padding-top: 2rem;
+  padding: 2rem 0;
+  overflow-y: auto;
 `;
 
 export const OrderList = styled.div`
@@ -50,37 +63,45 @@ export const Item = styled.div`
   display: flex;
   margin: 1rem 0;
   justify-content: space-between;
+  border-bottom: 2px dashed ${props => props.theme.gray};
 
   h2 {
-    margin: 0;
-    font-weight: 600;
-    font-size: 1.8rem;
-    color: ${props => props.theme.black};
+    margin: 0 1.5rem;
   }
+`;
 
-  h3 {
-    font-weight: 600;
-    font-size: 1.6rem;
-    margin: 0.3rem 0 0 3rem;
-    color: ${props => props.theme.black};
+export const ContainerClientsOrder = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 3rem 3rem;
+  border-top: 3px solid ${props => props.theme.gray};
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin-top: 1rem;
+  flex-direction: row;
+  align-items: center;
+  justify-content: start;
+
+  button:not(:last-child) {
+    margin-right: 1.5rem;
   }
 `;
 
 export const ContainerTotal = styled.div`
   width: 30%;
   display: flex;
-  height: 100vh;
   padding: 2rem 3rem;
   align-items: center;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: flex-start;
   background-color: ${props => props.theme.gray};
 
   #linha-horizontal {
-    margin-bottom: 35rem;
-    width: 25rem;
-    border: 1px solid ${props => props.theme.black};
+    width: 100%;
+    height: 2px;
     background-color: ${props => props.theme.black};
   }
 `;
-
