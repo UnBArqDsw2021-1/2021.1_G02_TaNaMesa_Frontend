ab7657b0817bc4e0c2334ef9576162f7b52ad4e1
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-26 15:48:48 -0300

    (#10) Adicionando páginas da Home com alguns estilos iniciais

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 
 import { useUserTheme } from 'hooks/theme';
 
-// import Loading from 'components/Loading';
 import NavBar from 'components/NavBar';
 import SideBar from 'components/SideBar';
+import BackgroundHamburguer from 'components/BackgroundHamburguer';
+// import Loading from 'components/Loading';
 
-import { Container, StyledButton } from 'pages/Home/styles';
+import { Container } from 'pages/Home/styles';
 
 const Home: React.FC = () => {
   const { switchTheme, theme } = useUserTheme();
 
   return (
-    <Container>
-      <NavBar center="Mesa" tableTitle="Mesa 1" />
-      <SideBar page="menu-user" />
-    </Container>
+    <>
+      <Container>
+        <SideBar page="menu-home" />
+        <BackgroundHamburguer text="Da mesa pra cozinha e da cozinha pra mesa." />
+        {/* <Loading /> */}
+      </Container>
+    </>
   );
 };
 
