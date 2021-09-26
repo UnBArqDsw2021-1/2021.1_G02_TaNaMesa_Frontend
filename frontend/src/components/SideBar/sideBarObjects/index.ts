c81cff8987d9484bc910408870553989bd0e881a
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-26 15:47:50 -0300

    (#10) Adicionando opções de botões do garçom

--- frontend/src/components/SideBar/sideBarObjects/index.ts
+++ frontend/src/components/SideBar/sideBarObjects/index.ts
@@ -1,3 +1,13 @@
-import menuUserButtons from './menuUserButtons';
+import menuUserButtons from 'components/SideBar/sideBarObjects/menuUserButtons';
+import homeButtons from 'components/SideBar/sideBarObjects/homeButtons';
+import waiterOptions from 'components/SideBar/sideBarObjects/waiterOptions';
+import kitchenOptions from 'components/SideBar/sideBarObjects/kitchenOptions';
+import managerOptions from 'components/SideBar/sideBarObjects/managerOptions';
 
-export default { menuUserButtons };
+export {
+  menuUserButtons,
+  homeButtons,
+  waiterOptions,
+  kitchenOptions,
+  managerOptions,
+};
---
+++ frontend/src/components/SideBar/sideBarObjects/waiterOptions.ts
@@ -0,0 +1,17 @@
+import imgViewOrders from 'assets/SideBar/visualizar_pedido 1.png';
+import imgDoubt from 'assets/SideBar/duvida 1.png';
+
+const homeButtons = [
+  {
+    id: 0,
+    icon: imgViewOrders,
+    text: 'Visualizar Pedidos das Mesas',
+  },
+  {
+    id: 1,
+    icon: imgDoubt,
+    text: 'Mesas que solicitam ajuda',
+  },
+];
+
+export default homeButtons;
