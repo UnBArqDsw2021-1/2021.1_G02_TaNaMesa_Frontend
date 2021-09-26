d9cdcf1ae62d40e9154612cd1fa8e29d5c3d6863
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-26 15:47:37 -0300

    (#10) Adicionando opções de botões do menu

--- frontend/src/components/SideBar/sideBarObjects/menuUserButtons.ts
+++ frontend/src/components/SideBar/sideBarObjects/menuUserButtons.ts
@@ -2,24 +2,35 @@ import imgBurguer from 'assets/SideBar/smbolo_hamburguer 1.png';
 import imgDrink from 'assets/SideBar/simbolo_bebida 1.png';
 import imgFries from 'assets/SideBar/simbolo batata 1.png';
 import imgDessert from 'assets/SideBar/simbolo_sobremesa 1.png';
+import imgWaiter from 'assets/SideBar/simbolo_usuario 2.png';
 
-const menuUserButtons = [
+const options = [
   {
+    id: 0,
     icon: imgBurguer,
     text: 'Burguers',
   },
   {
+    id: 1,
     icon: imgFries,
     text: 'Batatas',
   },
   {
+    id: 2,
     icon: imgDrink,
     text: 'Bebidas',
   },
   {
+    id: 3,
     icon: imgDessert,
     text: 'Sobremesas',
   },
 ];
 
-export default menuUserButtons;
+const callWaiter = {
+  id: 4,
+  icon: imgWaiter,
+  text: 'Chamar Garçom',
+};
+
+export default { options, callWaiter };
