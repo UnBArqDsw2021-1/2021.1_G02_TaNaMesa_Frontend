cf591584bfa90ecee02cbf0749201dad47d94dd3
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-28 18:49:58 -0300

    (#17) Adicionando tela de Menu responsiva buscando os dados da api

--- frontend/src/pages/Menu/index.tsx
+++ frontend/src/pages/Menu/index.tsx
@@ -1,34 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import NavBar from 'components/NavBar';
 import SideBar from 'components/SideBar';
 import MenuItem from 'components/MenuItem';
+import { useScreenSize } from 'hooks/screen';
 
 import { Container, MenuContainer } from 'pages/Menu/styles';
 
+import { getAllItems, Item } from 'services/items';
+
 const Menu: React.FC = () => {
+  const { openMenu } = useScreenSize();
+
+  const [menuItems, setMenuItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    getAllItems().then(response => {
+      setMenuItems(response);
+    });
+  }, []);
+
   return (
     <>
       <NavBar left="Voltar" center="Mesa" tableTitle="Mesa 1" right="Pedido" />
       <Container>
-        <SideBar page="menu-user" hasLogo />
+        <SideBar
+          collapse
+          size={openMenu ? 'large' : 'small'}
+          page="menu-user"
+          hasLogo
+        />
 
-        <MenuContainer>
+        <MenuContainer size={openMenu ? 'small' : 'large'}>
           <h1>Cardápio</h1>
-          {Array.from({ length: 10 }, (_, i) => i + 1).map(index => {
+
+          {menuItems.map(item => {
             return (
               <MenuItem
-                key={index}
+                key={item.idItem}
                 image="https://blog.finamac.com/wp-content/uploads/2019/10/309956-como-oferecer-os-melhores-sabores-de-milkshake-para-os-clientes-1280x640.jpg"
-                name="Milkshake"
-                price="16.90"
+                name={item.name}
+                price={String(item.price)}
                 // discount={0}
-                description="Disponível nos sabores morango, chocolate, oreo e creme de avelã."
+                description={`${item.description} ${item.notes}`}
               />
             );
           })}
         </MenuContainer>
-
         {/* <Loading /> */}
       </Container>
     </>
--- frontend/src/pages/Menu/styles.ts
+++ frontend/src/pages/Menu/styles.ts
@@ -1,21 +1,39 @@
 import styled from 'styled-components';
 
+interface MenuContainerProps {
+  size: 'large' | 'small';
+}
+
 export const Container = styled.div`
   display: flex;
-  height: 100vh;
+  width: 100%;
+  height: calc(100vh - 6rem);
+  /* position: absolute; */
+  /* margin: 0 auto; */
   flex-direction: row;
   background-color: ${props => props.theme.white};
+  transition: 1s ease;
 `;
 
-export const MenuContainer = styled.section`
+export const MenuContainer = styled.section<MenuContainerProps>`
   display: flex;
-  width: 100%;
-  overflow-y: auto;
   flex-direction: column;
-  background-color: ${props => props.theme.white};
+  width: ${props => (props.size === 'large' ? '93%' : '70%')};
+  height: 100%;
+  background-color: ${props => props.theme.background};
+  overflow-y: auto;
+  padding: 4rem 2.5rem;
+  transition: 1s ease;
 
   h1 {
+    font-weight: 600;
+    font-size: 4rem;
+    line-height: 4.9rem;
     color: ${props => props.theme.black};
-    margin: 2rem 0 0 4rem;
+    margin-bottom: 4rem;
+  }
+
+  @media (max-width: 768px) {
+    width: ${props => (props.size === 'large' ? '90%' : '55%')};
   }
 `;
