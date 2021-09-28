ddeae70d2b540049794878694de296375573483e
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-28 18:49:25 -0300

    (#17) Alterando cor de background do container da Home de acordo com protótipo

--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   height: 100vh;
   flex-direction: row;
-  background-color: ${props => props.theme.white};
+  background-color: ${props => props.theme.background};
 `;
 
 export const StyledButton = styled.button`
