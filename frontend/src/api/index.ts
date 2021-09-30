8d53e30818d7ba8292e8569a56d87037ef06b319
Lucas Boaventura <lucasxboaventura@hotmail.com>
2021-09-30 17:26:57 -0300

    (#17) Cria mock para token da api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:3333',
+  headers: {
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzMwMDQ3NDIsImV4cCI6MTYzMzA5MTE0Miwic3ViIjoiMTIzNCJ9.IKd3b_7XIndH_Jn1zxVlpxgIErMeFjDKzRCIUA_FnNU`,
+  },
 });
 
 export default api;
