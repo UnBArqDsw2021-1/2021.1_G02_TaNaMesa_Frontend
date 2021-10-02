e2005b9aa81824c8ec358d38ae3688aa0bef8e32
ServidioEC <52542729+ServidioEC@users.noreply.github.com>
2021-10-02 15:27:23 -0300

    (#33) Ajustando tela garçom ajuda
    Co-Authored-By: Emily Dias Sousa <emilydiassousa26@gmail.com>

--- frontend/src/pages/SolicitationWaiter/index.tsx
+++ frontend/src/pages/SolicitationWaiter/index.tsx
@@ -20,26 +20,64 @@ const SolicitationWaiter: React.FC = () => {
       id: '3',
       mesa: '12',
     },
+    {
+      id: '4',
+      mesa: '13',
+    },
+    {
+      id: '5',
+      mesa: '14',
+    },
+    {
+      id: '6',
+      mesa: '15',
+    },
+    {
+      id: '1',
+      mesa: '10',
+    },
+    {
+      id: '2',
+      mesa: '11',
+    },
+    {
+      id: '3',
+      mesa: '12',
+    },
+    {
+      id: '4',
+      mesa: '13',
+    },
+    {
+      id: '5',
+      mesa: '14',
+    },
+    {
+      id: '6',
+      mesa: '15',
+    },
   ];
 
   return (
     <>
       <NavBar left="Voltar" />
       <Container>
-        <SideBar page="menu-waiter" />
+        <SideBar page="menu-waiter" hasLogo />
         <div className="buttons">
           <div className="title">
             Clique sobre a mesa quando atender a solicitação de ajuda
           </div>
           {mesas.map(value => {
             return (
-              <HelpButton
-                key={value.id}
-                checked={checked}
-                onToggle={setChecked}
-              >
-                Mesa {value.mesa}
-              </HelpButton>
+              <div className="button-mesas">
+                <HelpButton
+                  key={value.id}
+                  checked={checked}
+                  onToggle={setChecked}
+                >
+                  Mesa {value.mesa}
+                </HelpButton>
+              </div>
             );
           })}
         </div>
